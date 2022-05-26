a687884472fe5a2e0374f48232c74261386726c4
Senior Farbod <40601333+farbod-norouzi@users.noreply.github.com>
2022-05-26 12:08:39 +0430

    Add files via upload

--- Etester.py
+++ Etester.py
@@ -1,11 +1,22 @@
+# Start (Import libraries)
+
 import os
 from os import system
 import smtplib
-import colorama 
-from colorama import Fore
 import platform 
+import sys
+try:
+    import colorama 
+    from colorama import Fore
+except:
+    print("Installing prerequisites")
+    system("pip install colorama")
+    exit('\n',"Run script Again")
 
-# Start APP
+# End (Import libraries)
+
+# Start (Banner & Clearing)
+
 def clear():
    result = platform.uname()[0]
    if result == "Windows":
@@ -19,7 +30,12 @@ print(Fore.RED+"""
 | __|  V  |/  \| | |    / _/ || | __/ _/ |/ / __| _ \ 
 | _|| \_/ | /\ | | |_  | \_| >< | _| \_|   <| _|| v / 
 |___|_| |_|_||_|_|___|  \__/_||_|___\__/_|\_\___|_|_\ """+Fore.RESET)
+
 colorama.init()
+
+# End (Banner & Clearing)
+
+# Start (APP & Get Input From User)
 
 def checker(rcpt) :
     gmail = smtplib.SMTP("gmail-smtp-in.l.google.com" , 25)
@@ -30,18 +46,26 @@ def checker(rcpt) :
     except smtplib.SMTPDataError:
         return True
 print('\n')
-gmaillist = input(Fore.CYAN+"Gmail List File : "+Fore.RESET)
-print('\n',"Scanning File :>",Fore.YELLOW+gmaillist+Fore.RESET,'\n')
-gmaillist = open(gmaillist)
+try:
+    gmaillist = input(Fore.CYAN+"Gmail List File : "+Fore.RESET)
+    print('\n',"Scanning File :>",Fore.YELLOW+gmaillist+Fore.RESET,'\n')
+    gmaillist = open(gmaillist)
+except:
+    print(Fore.RED+ "You canceled the program!"+Fore.RESET)
+    sys.exit()
+try:
+    for gmail in gmaillist:
+        gmail = gmail.strip("\n")
+        if checker(gmail):
+            print(colorama.Fore.GREEN + "[+] {} is Correct !".format(gmail))  
+    
+        else:
+            print(colorama.Fore.RED + "[-] {} is InCorrect !".format(gmail))
 
-for gmail in gmaillist:
-    gmail = gmail.strip("\n")
-    if checker(gmail):
-        print(colorama.Fore.GREEN + "[+] {} is Correct !".format(gmail))  
 
-    else:
-        print(colorama.Fore.RED + "[-] {} is InCorrect !".format(gmail))
+    input(Fore.RESET+"\n [!] Press Enter For Exit")
+except:
+    print(Fore.RED+ "You canceled the program!"+Fore.RESET)
 
-print(Fore.RESET+'\n',"[!] Press Enter For Exit",'\n')                  
-input()
+# End (APP & Get Input From User)
         
